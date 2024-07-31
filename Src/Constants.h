627623d64a91836b96b9516ae320d86b65b81997
Vítězslav Zotov <vzotov42@gmail.com>
2024-07-31 11:31:17 +0200

    Saving before the compute LOD

--- Src/Constants.h
+++ Src/Constants.h
@@ -5,4 +5,6 @@ namespace Constants
     constexpr const unsigned int MAX_MESHLET_VERTICES = 64;
     constexpr const unsigned int MAX_MESHLET_INDICES = 384;
     constexpr const unsigned int MAX_MESHLET_TRIANGLES = MAX_MESHLET_INDICES / 3;
+
+	constexpr const unsigned int MAX_LOD_LEVELS = 8;
 } // namespace Constants
